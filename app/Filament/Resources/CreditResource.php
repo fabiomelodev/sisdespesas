7ad502ca6363fd7161c88b6441b751fe877036ca
Fabio Melo <fabio.leandro@evolutap.com.br>
2024-12-14 08:31:55 -0300

    credit

---
+++ app/Filament/Resources/CreditResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CreditResource\Pages;
+use App\Filament\Resources\CreditResource\RelationManagers;
+use App\Models\Credit;
+use Filament\Forms;
+use Filament\Forms\Components\{Section, Select, TextInput};
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
+
+class CreditResource extends Resource
+{
+    protected static ?string $model = Credit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->columns(12)
+            ->schema([
+                Section::make()
+                    ->columnSpan(9)
+                    ->schema([
+                        TextInput::make('title')
+                            ->label('Título')
+                    ]),
+                Section::make()
+                    ->columnSpan(3)
+                    ->schema([
+                        TextInput::make('value')
+                            ->label('Limite')
+                            ->prefix('R$'),
+                        Select::make('bank_id')
+                            ->label('Banco')
+                            // ->relationship('bank', 'title', function (Builder $query) {
+                            //     $query->where('user_id', Auth::user()->id);
+                            // })
+                            ->required(fn(Get $get) => $get('type') == 'inconstante' ?: false),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCredits::route('/'),
+            'create' => Pages\CreateCredit::route('/create'),
+            'edit' => Pages\EditCredit::route('/{record}/edit'),
+        ];
+    }
+}
