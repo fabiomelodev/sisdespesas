d29402374cc9152400b97ab94deb4fa9bcb45386
Fabio Melo <fabio.leandro@evolutap.com.br>
2025-07-14 00:45:30 -0300

    fix(InvoiceResource): adjustment responsive pages list, create and edit

--- app/Filament/Resources/WarningResource.php
+++ app/Filament/Resources/WarningResource.php
@@ -27,7 +27,7 @@ class WarningResource extends Resource
 
     protected static ?string $navigationGroup = 'Avisos';
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 3;
 
     public static function getNavigationGroup(): ?string
     {
